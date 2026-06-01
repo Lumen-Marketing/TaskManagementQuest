04786fcb9e7d3415e53b44389f8c3fdfa487e60f
ShanIngrid1207 <aittagala@mymail.mapua.edu.ph>
2026-06-02 03:25:16 +0800

    Delete supabase directory

--- supabase/sql/010_revoke_role_helpers_from_anon.sql
+++
@@ -1,3 +0,0 @@
-revoke execute on function public.current_profile_role() from anon, public;
-revoke execute on function public.current_member_id() from anon, public;
-revoke execute on function public.can_manage_roles() from anon, public;
